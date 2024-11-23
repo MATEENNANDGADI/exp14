05e1bde0bf0fc397276c5ed7aa23243ed5b1b6b9
mateen <mateennandgadi@gmail.com>
2024-11-24 00:40:40 +0530

    okay

---
+++ About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return(
+        <div>
+            <p1>roooooebnbdh ujdfhebhe LL</p1>
+        </div>
+    )
+}
+
+export default About;
---
+++ App.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+
+
+function App(){
+  return(
+    <div classname ="App">
+      <h1>jdodf</h1>
+      <Link to='/About'>click it </Link>
+    </div>
+  )
+}
+
+export default App;
